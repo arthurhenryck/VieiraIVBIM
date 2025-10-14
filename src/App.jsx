beb9debe7c6e45f27d22af092a4b915940b07d27
Arthur henryck <arthur.henryck@hotmail.com>
2025-10-14 15:43:24 -0300

    primeiro envio

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,33 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <label htmlFor="CampNome"> Nome: </label>
+      <br />
+      <input type="text" placeholder="Digite seu nome" nome="CampNome" id='CampNome' />
+
+      <br />
+
+      <label htmlFor="CampIdade"> Idade: </label>
+      <br />
+      <input type="date" nome="CampIdade" id='CampIdade' />
+
+      <br />
+
+      <label htmlFor="CampTel"> Telefone: </label>
+      <br />
+      <input type="number" placeholder='Digite seu telefone' name="CampTel" id="CampTel" />
+
+      <br />
+
+      <a href="https://www.instagram.com/accounts/onetap/?next=%2F"> Instagram </a>
+
+      <br />
+
+      <a href="https://github.com/arthurhenryck"> Guthub </a>
+
+      <br />
+
+      
     </>
   )
 }
