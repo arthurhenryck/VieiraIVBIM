d957282ebb1feec7703b1463f068cbe22e89e765
Arthur Henryck Vieira <161501589+arthurhenryck@users.noreply.github.com>
2025-11-11 14:59:31 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [dados, setDados] = useState([]);
+
+useEffect(()=>{
+  fetch("https://api.alanleiser.com/")
+    .then(res => res .json())
+    .then(data =>console.log(data))
+},[])
+
+  const enviarFormas=(e)=>{
+    e.preventdefault()
+    fetch('https://api.alanleiser.com/user',{
+      method:'POST',
+      body:JSON.stringify({
+        "email": "",
+        "password":"" 
+      })
+    })
+    console.log(`Email: ${email},Senha: ${password}`)
+  }
+
+
+
+
 
   return (
     <>
       <label htmlFor="CampNome"> Nome: </label>
       <br />
-      <input type="text" placeholder="Digite seu nome" nome="CampNome" id='CampNome' />
+      <input type="text" placeholder="Digite seu nome" name="CampNome" id="CampNome" />
 
       <br />
 
       <label htmlFor="CampIdade"> Idade: </label>
       <br />
-      <input type="date" nome="CampIdade" id='CampIdade' />
+      <input type="number" placeholder="Digite sua idade" name="CampIdade" id="CampIdade" />
 
       <br />
 
       <label htmlFor="CampTel"> Telefone: </label>
       <br />
-      <input type="number" placeholder='Digite seu telefone' name="CampTel" id="CampTel" />
+      <input type="number" placeholder="Digite seu telefone" name="CampTel" id="CampTel" />
 
       <br />
 
-      <a href="https://www.instagram.com/accounts/onetap/?next=%2F"> Instagram </a>
-
+      <a href="https://www.instagram.com/accounts/onetap/?next=%2F">Instagram</a>
       <br />
+      <a href="https://github.com/arthurhenryck">Github</a>
 
-      <a href="https://github.com/arthurhenryck"> Guthub </a>
-
-      <br />
-
-      
     </>
-  )
+  );
 }
 
-export default App
+export default App;
